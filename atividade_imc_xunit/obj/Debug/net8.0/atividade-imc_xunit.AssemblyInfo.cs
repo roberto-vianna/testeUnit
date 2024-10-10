9f7004477783358dc2a897ea3a3125d4d92d5ec4
roberto-vianna <jrviana2021@gmail.com>
2024-10-10 16:30:04 -0300

    Execução de teste com XUNIT

--- atividade_imc_xunit/obj/Debug/net8.0/atividade-imc_xunit.AssemblyInfo.cs
+++ atividade_imc_xunit/obj/Debug/net8.0/atividade-imc_xunit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade-imc_xunit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6bd690fdc1182909e98df854f58b3b79a2ca19")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade-imc_xunit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade-imc_xunit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
